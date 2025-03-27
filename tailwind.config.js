3833c013a462954b4a1a819eb3698215dd94a831
karthikcsq <kthyagar@KarthikLGGram>
2025-03-27 01:20:17 -0400

    first

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}", // Ensure Tailwind scans your files for class usage
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          quicksand: ['"Quicksand"', 'sans-serif'], // Add Quicksand as a custom font
+        },
+      },
+    },
+    plugins: [],
+  };
